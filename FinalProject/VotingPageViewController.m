12f8e153d7ab5760a2e57844c876ee06f87bc5b9
Dimo <diramisu217@gmail.com>
2015-10-15 21:01:55 +0800

    1015

---
+++ FinalProject/VotingPageViewController.m
@@ -0,0 +1,37 @@
+//
+//  VotingPageViewController.m
+//  FinalProject
+//
+//  Created by 廖崇捷 on 2015/10/15.
+//  Copyright © 2015年 鄭涵嚴. All rights reserved.
+//
+
+#import "VotingPageViewController.h"
+
+@interface VotingPageViewController ()
+
+@end
+
+@implementation VotingPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
