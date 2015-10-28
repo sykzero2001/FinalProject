1a612a3eca9d2a43c1cc8898c80d7dd075e86bff
Dimo <diramisu217@gmail.com>
2015-10-28 09:36:41 +0800

    merge1028

---
+++ FinalProject/CongressmanTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CongressmanTableViewController.h
+//  FinalProject
+//
+//  Created by 廖崇捷 on 2015/10/27.
+//  Copyright © 2015年 鄭涵嚴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CongressmanTableViewController : UITableViewController
+
+@end
---
+++ FinalProject/PartyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PartyTableViewController.h
+//  FinalProject
+//
+//  Created by 廖崇捷 on 2015/10/27.
+//  Copyright © 2015年 鄭涵嚴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartyTableViewController : UITableViewController
+
+@end
