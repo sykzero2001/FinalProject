9e47e0b5224553a24981f664d2bb1d42f56b1a32
Dante <deathzeroking@hotmail.com>
2015-10-31 11:41:54 +0800

    merge Finish

--- FinalProject/IssueViewController.m
+++ FinalProject/IssueViewController.m
@@ -27,39 +27,39 @@
 
 //重新產生議題
 - (IBAction)resetBut:(id)sender {
-    [self getRandomIssue];
+//    [self getRandomIssue];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self getRandomIssue];
+//    [self getRandomIssue];
     self.random.layer.cornerRadius = self.random.bounds.size.width / 2.0;
 }
 
 
 //隨機產生議題
--(void) getRandomIssue {
-    mainArrayForBut = [[NSMutableArray alloc] init];
-    issueArray = @[@"農地", @"核四",@"服貿",@"吃飯",@"睡覺",@"打東東",@"哈囉",@"早安",@"午安"];
-    while (mainArrayForBut.count<5) {
-        NSUInteger randomIndex = arc4random() % [issueArray count];
-        NSString *contain = [NSString stringWithFormat:@"SELF contains[c] '%@'", issueArray[randomIndex]];
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:contain];
-        NSArray *checkDuplicate = [mainArrayForBut filteredArrayUsingPredicate:predicate];
-        long int lengthOfCheckDuplicate = checkDuplicate.count;
-        if (lengthOfCheckDuplicate == 0) {
-            [mainArrayForBut addObject:issueArray[randomIndex]];
-        };
-        NSLog(@"");
-    };
-    
-    for (NSUInteger i = 0; i < 5; i++){
-        UIButton *issues = self.issueCollectionBut[i];
-        issues.layer.cornerRadius = issues.bounds.size.width / 2.0;
-        [self.issueCollectionBut[i] setTitle:mainArrayForBut[i]  forState: UIControlStateNormal];
-        
-    }
-}
+//-(void) getRandomIssue {
+//    mainArrayForBut = [[NSMutableArray alloc] init];
+//    issueArray = @[@"農地", @"核四",@"服貿",@"吃飯",@"睡覺",@"打東東",@"哈囉",@"早安",@"午安"];
+//    while (mainArrayForBut.count<5) {
+//        NSUInteger randomIndex = arc4random() % [issueArray count];
+//        NSString *contain = [NSString stringWithFormat:@"SELF contains[c] '%@'", issueArray[randomIndex]];
+//        NSPredicate *predicate = [NSPredicate predicateWithFormat:contain];
+//        NSArray *checkDuplicate = [mainArrayForBut filteredArrayUsingPredicate:predicate];
+//        long int lengthOfCheckDuplicate = checkDuplicate.count;
+//        if (lengthOfCheckDuplicate == 0) {
+//            [mainArrayForBut addObject:issueArray[randomIndex]];
+//        };
+//        NSLog(@"");
+//    };
+//    
+//    for (NSUInteger i = 0; i < 5; i++){
+//        UIButton *issues = self.issueCollectionBut[i];
+//        issues.layer.cornerRadius = issues.bounds.size.width / 2.0;
+//        [self.issueCollectionBut[i] setTitle:mainArrayForBut[i]  forState: UIControlStateNormal];
+//        
+//    }
+//}
 //選擇按鍵後彈出對應議題的投票
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     UIButton *btn = sender;
